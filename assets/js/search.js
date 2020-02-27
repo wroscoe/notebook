ed917e97a22ec06eb2f8cd36d68fcb934986827e
wroscoe <wroscoe@users.noreply.github.com>
2020-02-27 16:58:20 +0000

    deploy: 49e0842fe5d934b4cb8cf0af5c0e9cb22e709721

--- assets/js/search.js
+++ assets/js/search.js
@@ -22,7 +22,7 @@ var documents = [{
     }, {
     "id": 4,
     "url": "https://www.wroscoe.com/2019/Economy_prediction_v0.html",
-    "title": "My prediction for the world economy.",
+    "title": "My prediction for the world economy (Oct 2019).",
     "body": "2019/10/16 - As an effort to hold my responsible for thinking rationally I’m publishing my prediction for the overall trend of the global economy. I know very little about economics so please don’t use this as investment advice. In the next 12 months we’ll enter a global recession caused by the boomers retiring. As the boomers retire they will begin liquidating their savings currently held in stocks and bonds. The stock market will fall and bond interest rates will rise. The higher interest rates will cause many leveraged companies to go bankrupt, further depressing the economy. The shrinking economy will reduce tax revenues for the US government and necessitate the treasury to sell $2-4 trillion worth of US bonds at a time when interest rates are already high. In an effort to keep interest rates low and to restart the economy, the US federal reserve will buy treasury bonds and other assets by printing money (ie quantitative easing). Major holders of US Treasury bonds which are foreign governments and pension funds will see that the US government can only inflate it’s way out of their debt liabilities so they will sell their bonds and US dollar reserves. This rush out of of US dollars will collapse it’s value relative to gold, bitcoin and hard assets. "
     }, {
     "id": 5,
